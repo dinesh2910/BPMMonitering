d7e5f916331d6fc49494246b756953cdf30c5f81
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2024-02-24 22:48:51 -0600

    Add files via upload

---
+++ BPMMonitering/BPMMonitering Watch App/BPMMoniteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMMoniteringApp.swift
+//  BPMMonitering Watch App
+//
+//  Created by Dinesh Danda on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BPMMonitering_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BPMMonitering/BPMMonitering/BPMMoniteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  BPMMoniteringApp.swift
+//  BPMMonitering
+//
+//  Created by Dinesh Danda on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct BPMMoniteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
